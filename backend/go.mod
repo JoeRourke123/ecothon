86637fb20ff6fc10b05c0f62de554687eac81a38
JoeRourke123 <joseph.rourke.2019@live.rhul.ac.uk>
2021-02-06 11:50:48 +0000

    Added a user model for the db

--- backend/go.mod
+++ backend/go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/gofiber/fiber v1.14.6
-	github.com/gofiber/fiber/v2 v2.0.0
+	github.com/gofiber/jwt/v2 v2.1.0
+	github.com/klauspost/compress v1.11.0 // indirect
 	go.mongodb.org/mongo-driver v1.4.1
 )
---
+++ backend/models/user.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type User struct {
+	_id primitive.ObjectID `json:"id,omitempty"`
+	FirstName	string `json:"firstname,omitempty"`
+	LastName	string `json:"lastname,omitempty"`
+	Email	string `json:"email,omitempty"`
+	Password	string `json:"password,omitempty,secret"`
+	LikedPosts []primitive.ObjectID `json:"liked_posts"`
+	Achievements []primitive.ObjectID `json:"achievements"`
+	StartingCarbon float32 `json:"carbon_estimate,omitempty"`
+	CurrentCarbon float32 `json:"current_estimate,omitempty"`
+	AccountCreated time.Time `json:"account_created,omitempty"`
+}
+
