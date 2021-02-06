c467506d11df49d6f7c22b4fcca40c57da962965
HoneyFox <gitea@honeyfox.uk>
2021-02-06 23:07:39 +0000

    Log requests to the server.

--- backend/main.go
+++ backend/main.go
@@ -5,12 +5,15 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 const port string = "0.0.0.0:3000"
 
 func main() {
 	app := fiber.New()
+
+	app.Use(logger.New())
 
 	router.SetupRoutes(app)
 
