615fcf0d76df128b5e9abdf0e6d90b4b2ec299ea
HoneyFox <gitea@honeyfox.uk>
2021-02-06 13:34:40 +0000

    Hash passwords before they are inserted into the database.

--- backend/endpoints/auth.go
+++ backend/endpoints/auth.go
@@ -5,9 +5,33 @@ import (
 	"ecothon/models"
 	"ecothon/utils"
 	"encoding/json"
+	"fmt"
+	"time"
 
 	"github.com/gofiber/fiber"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
 )
+
+func checkPass(checkPassword, hashedPassword []byte) bool {
+	error := bcrypt.CompareHashAndPassword(hashedPassword, checkPassword)
+
+	if error == nil {
+		return true
+	}
+
+	return false
+}
+
+func generateHash(pass []byte) string {
+	hash, err := bcrypt.GenerateFromPassword(pass, bcrypt.MinCost)
+
+	if err != nil {
+		fmt.Println("Error generating hash")
+	}
+
+	return string(hash)
+}
 
 func CreateUser(c *fiber.Ctx) {
 	collection, err := utils.GetMongoDbCollection("users")
@@ -18,6 +42,17 @@ func CreateUser(c *fiber.Ctx) {
 
 	var user models.User
 	json.Unmarshal([]byte(c.Body()), &user)
+
+	user.AccountCreated = time.Now()
+
+	if user.LikedPosts == nil {
+		user.LikedPosts = []primitive.ObjectID{}
+	}
+	if user.LikedPosts == nil {
+		user.LikedPosts = []primitive.ObjectID{}
+	}
+
+	user.Password = generateHash([]byte(user.Password))
 
 	res, err := collection.InsertOne(context.Background(), user)
 	if err != nil {
--- backend/go.mod
+++ backend/go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/gofiber/jwt/v2 v2.1.0
 	github.com/klauspost/compress v1.11.0 // indirect
 	go.mongodb.org/mongo-driver v1.4.1
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 )
