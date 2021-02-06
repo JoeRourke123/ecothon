6d18011d2603734ec21881b0121e6bf418da9748
seany067 <sean@escreet.co.uk>
2021-02-06 14:36:20 +0000

    Made the feed page refreshable and made the feed items look a bit better.

--- frontend/ecothon/lib/feed.dart
+++ frontend/ecothon/lib/feed.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:ecothon/FeedStore.dart';
 import 'package:provider/provider.dart';
@@ -10,46 +12,72 @@ class FeedPage extends StatefulWidget {
   _FeedPageState createState() => _FeedPageState();
 }
 
-class _FeedPageState extends State<FeedPage> {
+class _FeedPageState extends State<FeedPage>
+    with AutomaticKeepAliveClientMixin<FeedPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("Feed"),
         ),
-        body: ListView.separated(
-          padding: EdgeInsets.all(8),
-          itemCount: Provider.of<FeedStore>(context).feedItemData.length,
-          itemBuilder: (context, index) {
-            return Consumer<FeedStore>(
-              builder: (context, feed, child) {
-                String text = feed.feedItemData[index].text;
-                return FeedItem(text: text);
+        body: RefreshIndicator(
+            onRefresh: _pullRefresh,
+            child: ListView.separated(
+              padding: EdgeInsets.all(8),
+              itemCount: Provider.of<FeedStore>(context).feedItemData.length,
+              itemBuilder: (context, index) {
+                return Consumer<FeedStore>(
+                  builder: (context, feed, child) {
+                    return FeedItem(
+                        text: feed.feedItemData[index].text,
+                        index: index,
+                    );
+                  },
+                );
               },
-            );
-          },
-          separatorBuilder: (BuildContext context, int index) =>
-              const Divider(),
-        ));
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(),
+            )));
   }
+
+  Future<void> _pullRefresh() async {
+    // Get new feed here by making request to http endpoint then create list
+    // of FeedItemData in items
+    List<FeedItemData> items = [
+      FeedItemData("New item 1"),
+      FeedItemData("New item 2"),
+      FeedItemData("New item 3"),
+      FeedItemData("New item 4"),
+    ];
+    Provider.of<FeedStore>(context, listen: false).setFeedItems(items);
+  }
+
+  @override
+  bool get wantKeepAlive => true;
 }
 
 class FeedItem extends StatelessWidget {
+  final int index;
   final String text;
+  final rng = new Random();
 
-  FeedItem({Key key, this.text}) : super(key: key);
+  List colors = Colors.primaries;
+
+  FeedItem({Key key, this.text, this.index}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    int rand = rng.nextInt(colors.length);
     return Container(
       padding: EdgeInsets.all(8),
-      child: Center(child: Text(this.text)),
+      child: Center(child: Text("\n\n" + this.text + "\n\n")),
       decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.blueAccent,
-        ),
+        border: Border.all(color: Colors.transparent),
         borderRadius: BorderRadius.all(Radius.circular(20)),
-        color: Colors.lightBlue,
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [this.colors[rand][100], this.colors[rand][900]])
       ),
     );
   }
