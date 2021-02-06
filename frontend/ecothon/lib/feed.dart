f0cdaae0b15f2fb936f3c2d7efde72bd7b00e560
seany067 <sean@escreet.co.uk>
2021-02-06 15:52:16 +0000

    Feed now uses http request to get data to populate the feed.

--- frontend/ecothon/lib/feed.dart
+++ frontend/ecothon/lib/feed.dart
@@ -1,8 +1,9 @@
+import 'dart:convert';
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:ecothon/FeedStore.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 class FeedPage extends StatefulWidget {
   FeedPage({Key key, this.title}) : super(key: key);
@@ -14,8 +15,11 @@ class FeedPage extends StatefulWidget {
 
 class _FeedPageState extends State<FeedPage>
     with AutomaticKeepAliveClientMixin<FeedPage> {
+  bool err = false;
+
   @override
   Widget build(BuildContext context) {
+    var snackbar = SnackBar(content: Text("Failed to fetch feed. Please retry"));
     return Scaffold(
         appBar: AppBar(
           title: Text("Feed"),
@@ -26,11 +30,14 @@ class _FeedPageState extends State<FeedPage>
               padding: EdgeInsets.all(8),
               itemCount: Provider.of<FeedStore>(context).feedItemData.length,
               itemBuilder: (context, index) {
+                if (err) {
+                  Scaffold.of(context).showSnackBar(snackbar);
+                }
                 return Consumer<FeedStore>(
                   builder: (context, feed, child) {
                     return FeedItem(
-                        text: feed.feedItemData[index].text,
-                        index: index,
+                      data: feed.feedItemData[index],
+                      index: index,
                     );
                   },
                 );
@@ -41,15 +48,18 @@ class _FeedPageState extends State<FeedPage>
   }
 
   Future<void> _pullRefresh() async {
-    // Get new feed here by making request to http endpoint then create list
-    // of FeedItemData in items
-    List<FeedItemData> items = [
-      FeedItemData("New item 1"),
-      FeedItemData("New item 2"),
-      FeedItemData("New item 3"),
-      FeedItemData("New item 4"),
-    ];
-    Provider.of<FeedStore>(context, listen: false).setFeedItems(items);
+    List<FeedItemData> items = [];
+    http.Response res = await http.get('http://ecothon.space/api/all');
+    if (res.statusCode == 200) {
+      err = false;
+      var decoded = json.decode(res.body);
+      for (var i in decoded) {
+        items.add(FeedItemData.fromJson(i));
+      }
+      Provider.of<FeedStore>(context, listen: false).setFeedItems(items);
+    } else {
+      err = true;
+    }
   }
 
   @override
@@ -58,34 +68,60 @@ class _FeedPageState extends State<FeedPage>
 
 class FeedItem extends StatelessWidget {
   final int index;
-  final String text;
+  final FeedItemData data;
   final rng = new Random();
 
   List colors = Colors.primaries;
 
-  FeedItem({Key key, this.text, this.index}) : super(key: key);
+  FeedItem({Key key, this.data, this.index}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     int rand = rng.nextInt(colors.length);
     return Container(
       padding: EdgeInsets.all(8),
-      child: Center(child: Text("\n\n" + this.text + "\n\n")),
+      child: Center(child: Text(this.data.user + "\n" + this.data.achievement)),
       decoration: BoxDecoration(
-        border: Border.all(color: Colors.transparent),
-        borderRadius: BorderRadius.all(Radius.circular(20)),
-        gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [this.colors[rand][100], this.colors[rand][900]])
-      ),
+          border: Border.all(color: Colors.transparent),
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [this.colors[rand][100], this.colors[rand][900]])),
     );
   }
 }
 
 @immutable
 class FeedItemData {
-  final String text;
+  final String user;
+  final String achievement;
+  final String type;
+  final String comments;
+  final String likedBy;
+  final String location;
+  final String details;
+  final DateTime createdAt;
 
-  FeedItemData(this.text) {}
+  FeedItemData(
+      {this.user,
+      this.achievement,
+      this.type,
+      this.comments,
+      this.likedBy,
+      this.location,
+      this.details,
+      this.createdAt});
+
+  factory FeedItemData.fromJson(Map<String, dynamic> json) {
+    return FeedItemData(
+        user: json["user"],
+        achievement: json["achievement"],
+        type: json["type"],
+        comments: json["comments"],
+        likedBy: json["liked_by"],
+        location: json["geolocation"],
+        details: json["details"],
+        createdAt: json["created_at"]);
+  }
 }
