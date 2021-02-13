30d4bbd25be8c8e4e67245f70ba586564d39454c
HoneyFox <gitea@honeyfox.uk>
2021-02-13 00:05:32 +0000

    Log to a file.

--- backend/main.go
+++ backend/main.go
@@ -3,18 +3,27 @@ package main
 import (
 	"ecothon/router"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
-const port string = "0.0.0.0:3000"
+const port string = "localhost:4000"
 
 func main() {
 	app := fiber.New(fiber.Config{ServerHeader: "just-a-hapi-guy/2.0"})
 
-	app.Use(logger.New())
+	file, err := os.OpenFile("./ecothon.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+   		log.Fatalf("error opening file: %v", err)
+	}
+	defer file.Close()
+
+	app.Use(logger.New(logger.Config{
+    	Output: file,
+	}))
 	app.Use(compress.New())
 
 	app.Static("/", "./static")
