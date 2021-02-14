ec30a2bc2307a122fcff0eb755b19795d38a9845
HoneyFox <gitea@honeyfox.uk>
2021-02-14 18:45:21 +0000

    As server is behind Nginx proxy, we need to use X-Real-IP in prod.

--- backend/main.go
+++ backend/main.go
@@ -13,11 +13,24 @@ import (
 const port string = "localhost:4000"
 
 func main() {
-	app := fiber.New(
-		fiber.Config{
-			ServerHeader: "ecothon-server/1.0",
-			BodyLimit:    10 * 1024 * 1024,
-		})
+	config := fiber.Config{
+		ServerHeader: "ecothon-server/1.0",
+		BodyLimit:    10 * 1024 * 1024,
+	}
+
+	requiredEnvVars := []string{"ECOTHON_DB_PASS", "ECOTHON_SPACES_KEY", "ECOTHON_SPACES_SECRET"}
+
+	for _, envVar := range requiredEnvVars {
+		if _, exists := os.LookupEnv(envVar); !exists {
+			log.Fatalf("Missing environment variable: %s", envVar)
+		}
+	}
+
+	if os.Getenv("ECOTHON_PROD") == "TRUE" {
+		config.ProxyHeader = "X-Real-IP"
+	}
+
+	app := fiber.New(config)
 
 	file, err := os.OpenFile("./ecothon.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
